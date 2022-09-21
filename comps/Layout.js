a34a7fa1f0ee62cd2c8f61c8320c9d7de999cdc3
dhruv-suraj <77670294+dhruv-suraj@users.noreply.github.com>
2022-09-21 14:07:11 +0530

    Update Layout.js

--- comps/Layout.js
+++ comps/Layout.js
@@ -1,6 +1,12 @@
 import Navbar from "./Navbar"
 import Footer from "./Footer"
+<meta
 
+      name="description"
+
+      content="Travello provides you with an opportunity to travel to the destinations you always longed for, join this amazing journey."
+
+    />
 const  Layout= ({children}) => {
     return ( 
         <div className="content">
