e3bae4331783988f50914396401747dae3391874
dhruv-suraj <77670294+dhruv-suraj@users.noreply.github.com>
2022-09-21 14:05:48 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import Layout from '../comps/Layout'
 import '../styles/globals.css'
+<meta
+
+      name="description"
+
+      content="Travello provides you with an opportunity to travel to the destinations you always longed for, join this amazing journey."
+
+    />
 
 function MyApp({ Component, pageProps }) {
   return (
